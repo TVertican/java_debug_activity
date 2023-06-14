e7dc394a788502a2410259459aebc185da798333
Tiffany P <tvertican@gmail.com>
2023-06-14 17:15:08 -0400

    finished Debug Activity

--- src/DebugActivity.java
+++ src/DebugActivity.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class DebugActivity {
 
     public static int divideNumbers(int a, int b) {
@@ -11,25 +13,28 @@ public class DebugActivity {
         System.out.println("Starting the program...");
 
 //            figure out how to "catch" the "divide by zero" error, don't just change the value on "y"
-        int result = divideNumbers(x, y)
-        System.out.println("Result: " + result);
-
-        String name = "John"
-        int length = name.length;
+        try {
+            int result = divideNumbers(x, y);
+            System.out.println("Result: " + result);
+        }catch (ArithmeticException error) {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+        String name = "John";
+        int length = name.length();
         System.out.println("Length of name: " + length);
 
-        int numbers = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= numbers.length; i++) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int i = 0; i < numbers.length; i++) {
 //                for loop should print one number each iteration
-            System.out.println(numbers);
+            System.out.println(numbers[i]);
         }
 
-        String[] fruits = ["Apple", "Banana", "Orange"];
-        for (String fruit : fruit) {
-            System.out.println(fruit)
+        String[] fruits = {"Apple", "Banana", "Orange"};
+        for (String fruit : fruits) {
+            System.out.println(fruit);
         }
 
-        System.out.println('End of the program.');
+        System.out.println("End of the program.");
     }
 
 }
